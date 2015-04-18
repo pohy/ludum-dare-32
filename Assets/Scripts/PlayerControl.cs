3edf6903ec4d1158a567d909674c0e6383782c3a
David Pohan <pohandav@gmail.com>
2015-04-18 05:41:52 +0200

    player control script and testing sprite, testing background sprite

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour
+{
+    [SerializeField]
+    private float movementSpeed;
+
+    private Rigidbody2D rigidBody;
+
+    private Vector2 movement;
+
+    // Use this for initialization
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        rigidBody.velocity = movement * movementSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxis("Horizontal");
+        movement.y = Input.GetAxis("Vertical");
+    }
+}
